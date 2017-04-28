d92116461c597ce67c65b30bfbd8e2e920009312
Frédéric Boyer <fboyer@gmail.com>
2017-04-28 11:37:06 -0400

    Support straight flush hand evaluation

--- lib/pokerwars/hand.ex
+++ lib/pokerwars/hand.ex
@@ -2,8 +2,29 @@ defmodule Pokerwars.Hand do
   alias Pokerwars.Card
 
   def score(cards) do
-    evaluate(cards)
+    sorted_cards = Enum.sort(cards, fn c, n -> c.rank <= n.rank end)
+    evaluate(sorted_cards)
   end
 
-  defp evaluate(_), do: :high_card
+  defp evaluate(cards) do
+    suits = Enum.map(cards, fn card -> card.suit end)
+    ranks = Enum.map(cards, fn card -> card.rank end)
+
+    cond do
+      same_suit?(suits) && consecutive_ranks?(ranks) ->
+        :straight_flush
+      true ->
+        :high_card
+    end
+  end
+
+  def same_suit?(suits) do
+    first_suit = Enum.fetch!(suits, 0)
+    Enum.all?(suits, fn suit -> suit == first_suit end)
+  end
+
+  def consecutive_ranks?([cur_rank, next_rank | rem_ranks]) do 
+    next_rank - cur_rank == 1 && consecutive_ranks?([next_rank | rem_ranks])
+  end
+  def consecutive_ranks?(_), do: true
 end
--- test/pokerwars/hand_test.exs
+++ test/pokerwars/hand_test.exs
@@ -4,51 +4,131 @@ defmodule Pokerwars.HandTest do
   alias Pokerwars.Card
   doctest Pokerwars.Hand
 
-  test "evaluates royal flush of hearts" do
-    cards = [
-      %Card{rank: 10, suit: :hearts},
-      %Card{rank: 11, suit: :hearts},
-      %Card{rank: 12, suit: :hearts},
-      %Card{rank: 13, suit: :hearts},
-      %Card{rank: 14, suit: :hearts}
-    ]
+  describe "score/1" do
+    @tag :skip
+    test "evaluates royal flush of hearts" do
+      cards = [
+        %Card{rank: 10, suit: :hearts},
+        %Card{rank: 11, suit: :hearts},
+        %Card{rank: 12, suit: :hearts},
+        %Card{rank: 13, suit: :hearts},
+        %Card{rank: 14, suit: :hearts}
+      ]
 
-    assert Hand.score(cards) == :royal_flush
+      assert Hand.score(cards) == :royal_flush
+    end
+
+    @tag :skip
+    test "evaluates royal flush of spades" do
+      cards = [
+        %Card{rank: 10, suit: :spades},
+        %Card{rank: 11, suit: :spades},
+        %Card{rank: 12, suit: :spades},
+        %Card{rank: 13, suit: :spades},
+        %Card{rank: 14, suit: :spades}
+      ]
+
+      assert Hand.score(cards) == :royal_flush
+    end
+
+    test "does not evaluate royal flush for mismatching suits" do
+      cards = [
+        %Card{rank: 10, suit: :diamonds},
+        %Card{rank: 11, suit: :diamonds},
+        %Card{rank: 12, suit: :diamonds},
+        %Card{rank: 13, suit: :clubs},
+        %Card{rank: 14, suit: :clubs}
+      ]
+
+      assert Hand.score(cards) != :royal_flush
+    end
+
+    test "evaluates straight flush of clubs" do
+      cards = [
+        %Card{rank: 7, suit: :clubs},
+        %Card{rank: 8, suit: :clubs},
+        %Card{rank: 9, suit: :clubs},
+        %Card{rank: 10, suit: :clubs},
+        %Card{rank: 11, suit: :clubs}
+      ]
+
+      assert Hand.score(cards) == :straight_flush
+    end
+
+    test "does not evaluate straight flush for non consecutive values" do
+      cards = [
+        %Card{rank: 7, suit: :clubs},
+        %Card{rank: 8, suit: :clubs},
+        %Card{rank: 9, suit: :clubs},
+        %Card{rank: 10, suit: :clubs},
+        %Card{rank: 2, suit: :clubs}
+      ]
+
+      refute Hand.score(cards) == :straight_flush
+    end
+
+    test "does not evaluate straight flush for mismatching suits" do
+      cards = [
+        %Card{rank: 7, suit: :clubs},
+        %Card{rank: 8, suit: :clubs},
+        %Card{rank: 9, suit: :spades},
+        %Card{rank: 10, suit: :clubs},
+        %Card{rank: 11, suit: :clubs}
+      ]
+
+      refute Hand.score(cards) == :straight_flush
+    end
+
+    test "evaluates high card" do
+      cards = [
+        %Card{rank: 2, suit: :spades},
+        %Card{rank: 3, suit: :hearts},
+        %Card{rank: 5, suit: :diamonds},
+        %Card{rank: 7, suit: :spades},
+        %Card{rank: 11, suit: :spades}
+      ]
+
+      assert Hand.score(cards) == :high_card
+    end
   end
 
-  test "evaluates royal flush of spades" do
-    cards = [
-      %Card{rank: 10, suit: :spades},
-      %Card{rank: 11, suit: :spades},
-      %Card{rank: 12, suit: :spades},
-      %Card{rank: 13, suit: :spades},
-      %Card{rank: 14, suit: :spades}
-    ]
+  describe "same suit?/1" do
+    test "returns true when all suits are the same" do
+      suits = [
+        :hearts,
+        :hearts,
+        :hearts,
+        :hearts,
+        :hearts
+      ]
 
-    assert Hand.score(cards) == :royal_flush
+      assert Hand.same_suit?(suits)
+    end
+
+    test "returns false when suits are not the same" do
+      suits = [
+        :hearts,
+        :hearts,
+        :hearts,
+        :hearts,
+        :diamonds
+      ]
+
+      refute Hand.same_suit?(suits)
+    end
   end
 
-  test "does not evaluate royal flush for mismatching suits" do
-    cards = [
-      %Card{rank: 10, suit: :diamonds},
-      %Card{rank: 11, suit: :diamonds},
-      %Card{rank: 12, suit: :diamonds},
-      %Card{rank: 13, suit: :clubs},
-      %Card{rank: 14, suit: :clubs}
-    ]
+  describe "consecutive_ranks?/1" do
+    test "returns true when ranks are consecutive" do
+      ranks = [1, 2, 3, 4, 5]
+      
+      assert Hand.consecutive_ranks?(ranks)
+    end
 
-    assert Hand.score(cards) != :royal_flush
-  end
+    test "returns false when ranks are not consecutive" do
+      ranks = [1, 2, 3, 5, 6]
 
-  test "evaluates high card" do
-    cards = [
-      %Card{rank: 2, suit: :spades},
-      %Card{rank: 3, suit: :hearts},
-      %Card{rank: 5, suit: :diamonds},
-      %Card{rank: 7, suit: :spades},
-      %Card{rank: 11, suit: :spades}
-    ]
-
-    assert Hand.score(cards) == :high_card
+      refute Hand.consecutive_ranks?(ranks)
+    end
   end
 end
